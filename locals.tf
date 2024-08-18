5d725930ee2e0eb10d5004d29044cacea7bf09d5
Tin <tin@Tins-MacBook-Air.local>
2024-08-18 22:40:04 +0530

    First commit

---
+++ locals.tf
@@ -0,0 +1,9 @@
+locals {
+  vpc_name              = "main-vpc"
+  public_subnet_name    = "public-subnet"
+  private_subnet_name   = "private-subnet"
+  igw_name              = "main-igw"
+  public_route_name     = "public-route-table"
+  public_instance_name  = "public-instance"
+  private_instance_name = "private-instance"
+}
